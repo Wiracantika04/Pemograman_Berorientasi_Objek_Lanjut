287cf8d8bcf26544b4ba9dd0103244e9dcd17280
Wiracantika04 <128268325+Wiracantika04@users.noreply.github.com>
2023-03-19 10:03:26 +0700

    delete buku.py

--- buku.py
+++
@@ -1,9 +0,0 @@
-class Buku:
-    def __init__(self, judul, penulis, tahunterbit):
-        self.judul = judul
-        self.penulis = penulis
-        self.tahunterbit = tahunterbit
-    def info(self):
-        print(f"Judul: {self.judul}\nPenulis: {self.penulis}\nTahun Terbit: {self.tahunterbit}")
-bukuA = Buku("Belajar Permograman Python", "Wira Cantika", "terbit tahun 2022")
-bukuA.info()
