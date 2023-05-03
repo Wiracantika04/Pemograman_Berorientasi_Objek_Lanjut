ede4ddf56173c879456c82813a44d3a8df76dded
Wiracantika04 <128268325+Wiracantika04@users.noreply.github.com>
2023-05-04 01:15:22 +0700

    Add files via upload
    Signed-off-by: Wiracantika04 <128268325+Wiracantika04@users.noreply.github.com>

---
+++ Tugas5/StopAsyncIteration.py
@@ -0,0 +1,20 @@
+import asyncio
+
+
+async def countdown(num):
+    while num > 0:
+        print(num)
+        await asyncio.sleep(1)
+        num -= 1
+
+    raise StopAsyncIteration
+
+
+async def main():
+    try:
+        await countdown(5)
+    except StopAsyncIteration:
+        print("Countdown stopped!")
+
+
+asyncio.run(main())
---
+++ Tugas5/SystemExit.py
@@ -0,0 +1,8 @@
+import sys
+
+try:
+    print("Program dimulai")
+    sys.exit()
+    print("Program selesai")
+except SystemExit:
+    print("Program dihentikan")
